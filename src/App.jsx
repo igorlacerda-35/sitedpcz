01e4b2e5b999e0306bb4fa0cb581ada4754153ec
igorlacerda-35 <igorlimaal@gmail.com>
2025-06-11 17:34:36 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <h1 className="text-3xl font-bold text-green-600">Bem-vindo ao DPCZ 🚀</h1>
+    </div>
+  )
+}
+
+export default App
