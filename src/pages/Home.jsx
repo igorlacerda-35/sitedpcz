2ffb00270e94bef0abc2145bb449e589bf0aa02f
igorlacerda-35 <igorlimaal@gmail.com>
2025-06-12 14:34:45 +0000

    limpeza do frontend

--- src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">Página Inicial</h1>
-      <p>Bem-vindo ao site da DPC!</p>
-    </div>
-  );
-}
