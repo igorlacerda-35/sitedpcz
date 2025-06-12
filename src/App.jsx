00adc22a80f00220df728acf2f3372a43be0c049
igorlacerda-35 <igorlimaal@gmail.com>
2025-06-12 15:10:36 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { useState } from "react";
+import Login from "./pages/Login";
+import Painel from "./pages/Painel";
+
+function App() {
+  const [logado, setLogado] = useState(false);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login setLogado={setLogado} />} />
+        <Route
+          path="/painel"
+          element={logado ? <Painel /> : <Navigate to="/" replace />}
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
