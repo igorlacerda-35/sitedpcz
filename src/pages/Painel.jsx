ab117e6a34905d800ec5e3a94cdec06ef9192f69
igorlacerda-35 <igorlimaal@gmail.com>
2025-06-12 11:48:18 -0300

    Add files via upload

---
+++ src/pages/Painel.jsx
@@ -0,0 +1,8 @@
+export default function Painel() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Painel do Cliente</h1>
+      <p>Aqui você poderá gerenciar seu projeto, como os números autorizados.</p>
+    </div>
+  );
+}
